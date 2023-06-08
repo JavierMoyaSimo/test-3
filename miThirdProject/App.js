13577557e4cc6df59896fe4bcf369b6dfdd9ddb5
Javi <javiermoyasimo@gmail.com>
2023-06-08 16:01:13 +0200

    feat: clone component App. Styles added

--- miThirdProject/App.js
+++ miThirdProject/App.js
@@ -1,22 +1,52 @@
 import { StyleSheet } from "react-native";
 //New imports
 import * as React from "react";
+import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//Components
 import HomeScreen from "./components/HomeScreen";
 import DetailScreen from "./components/DetailScreen";
+//Redux
 import { Provider } from "react-redux";
 import store from "./app/store";
 
+//To navigate
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  //Normal mode- dark mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  //Handle-button
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <Provider store={store}>
       <NavigationContainer>
+        <div
+          className={
+            darkMode
+              ? [styles.dark, styles.button]
+              : [styles.light, styles.button]
+          }
+          onClick={handleDarkMode}
+        >
+          {darkMode ? "To Day!" : "To Night!"}
+        </div>
         <Stack.Navigator>
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="DetailScreen" component={DetailScreen} />
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            style={darkMode ? styles.light : styles.dark}
+          />
+          <Stack.Screen
+            name="DetailScreen"
+            component={DetailScreen}
+            style={darkMode ? styles.light : styles.dark}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
@@ -29,5 +59,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  light: {
+    backgroundColor: "rgba(149, 174, 216, 0.215)",
+    color: "black",
+  },
+  dark: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  button: {
+    height: "5%",
+    textAlign: "center",
+    cursor: "pointer",
   },
 });
